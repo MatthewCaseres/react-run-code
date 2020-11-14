7f63bcc601454a0794cabf3abb76c8e4f484fad5
Matthew Caseres <matthewcaseres@outlook.com>
2020-11-14 15:42:45 -0600

    Update index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import Editor from '../dist/index';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Editor id="10" modelsInfo={[]} />
     </div>
   );
 };
