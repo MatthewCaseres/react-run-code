f9e42314918126f8e9d586ee6c8975faf6d24950
Matthew Caseres <matthewcaseres@outlook.com>
2020-11-14 13:34:12 -0600

    Stricter imports rules
    User is restricted to certain styles of exports. Imports and exports have more accurate regex when stripping for bundle.

--- src/editor/utils/runFile.ts
+++ src/editor/utils/runFile.ts
@@ -6,13 +6,27 @@ function getErrors(
   monacoInstance: typeof monacoForType,
   orderedModels: editor.ITextModel[]
 ) {
+  //Get model decoration errors
   const ranPaths = orderedModels.map(textModel => textModel.uri.path);
   const ranModelMarkers = monacoInstance.editor
     .getModelMarkers({})
     .filter(markers => ranPaths.includes(markers.resource.path));
-  const messages = ranModelMarkers
+  let messages = ranModelMarkers
     .filter(marker => marker.severity === 8)
     .map(model => `${model.resource.path.substr(1)}: ${model.message}`);
+
+  //Get import/export formatting errors
+  console.log(JSON.stringify(orderedModels[0].getValue()));
+  orderedModels.map(textModel => {
+    const errors = textModel.getValue().match(/export\s+[^{]+/);
+    const errorsFormatted = errors?.map(
+      error =>
+        `Error: '${error}', this editor only supports named exports contained in a single line. Like 'export {thing1, thing2}`
+    );
+    if (errorsFormatted) {
+      messages = [...messages, ...errorsFormatted];
+    }
+  });
   return messages;
 }
 
@@ -35,15 +49,20 @@ export default async function runFile(
     //If type errors log and don't run files.
     const errors = getErrors(monacoInstance, ranModels);
     errors.map(error => console.error(error, editorId));
-    //Concat files left to right
+    //Concat files
     if (!errors.length) {
       const lines =
         ranModels
-          .map(model => model.getLinesContent()) //text array for each model
-          .reduce((prevModels, model) => prevModels.concat(model)) //flatten
-          .filter(line => !line.includes('import') && !line.includes('export')) //no imports and exports, expect separate line for declarations
-          .map(line => line + '\n')
-          .reduce((prevLines, line) => prevLines + line) + 'export {}'; //Prevents duplicate identifiers by making module;
+          .map(model =>
+            model
+              .getValue()
+              .replace(
+                /import\s+?(?:(?:(?:[\w*\s{},]*)\s+from\s+?)|)(?:(?:".*?")|(?:'.*?'))[\s]*?(?:;|$|)/g,
+                ''
+              )
+              .replace(/export\s+{[^}]*}/g,'')
+          ) //text array for each model
+          .reduce((prevLines, line) => prevLines + line) + '\r\nexport {}'; //Prevents duplicate identifiers by making module;
       //Set runner model value as concatted text
       runnerModel.setValue(lines);
 
@@ -53,7 +72,7 @@ export default async function runFile(
       const emittedJS = (
         await tsClient.getEmitOutput(runnerModel.uri.toString())
       ).outputFiles[0].text.replace(/export {};\r\n/, '');
-      
+
       let consoleOverride = `let console = (function (oldCons) {
         return {
           ...oldCons,
